31960895829ed7e68927c65bf6fb9fce656cedd6
DJesusCry <lorenzo_98@hotmail.fr>
2021-01-20 09:24:14 +0100

    Front End Skeleton

--- SMI-Frontend/src/app/app.module.ts
+++ SMI-Frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ConfigComponent } from './config/config.component';
+import { EzconfigComponent } from './ezconfig/ezconfig.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ConfigComponent,
+    EzconfigComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
