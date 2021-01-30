97a79534951812b4d17fcb0d19ee8eeca6cfb716
DJesusCry <lorenzo_98@hotmail.fr>
2021-01-30 17:27:03 +0100

    config avancée

--- SMI-Frontend/src/app/config/config.component.ts
+++ SMI-Frontend/src/app/config/config.component.ts
@@ -9,12 +9,66 @@ import { FormBuilder } from "@angular/forms";
 export class ConfigComponent implements OnInit {
   configForm = this.formBuilder.group({
     name: '', 
-    address: ''
+    budget: '',
+    mounted: '',
+    proc: '',
+    motherBoard: '',
+    RAM: '',
+    coolingSystem: '',
+    GPU: '',
+    SSD1: '',
+    SSD2: '',
+    HDD1: '',
+    HDD2: '',
+    cdPlayerBurner: '',
+    cardsPlayer: '',
+    wifiBoard: '',
+    case: '',
+    display1: '',
+    display2: '',
+    powerUnit: '',
+    keyBoard: '',
+    mouse: '',
+    OS: '',
+    antivirus: '',
+    microsoftOffice: '',
+    inverter: '',
+    message:'',
+    soundBoard:'',
   })
+
+  mbCheck = false;
+  procCheck = false;
+  gpuCheck = false;
+  ramCheck = false;
+  coolingCheck = false;
+  powerCheck = false;
+  ssd1Check = false;
+  ssd2Check = false;
+  hdd1Check = false;
+  hdd2Check = false;
+  wifiCheck = false;
+  soundCheck = false;
+  caseCheck = false;
+  cdCheck = false;
+  cardsCheck = false;
+  display1Check = false;
+  display2Check = false;
+  keybordCheck = false;
+  mouseCheck = false;
+  inverterCheck = false;
+  osCheck = false;
+  virusCheck = false;
+  officeCheck = false;
+  
 
   constructor(
     private formBuilder: FormBuilder
   ) {}
+
+  onSubmit(): void {
+    console.log(this.configForm.value)
+  }
 
   ngOnInit(): void {
   }
