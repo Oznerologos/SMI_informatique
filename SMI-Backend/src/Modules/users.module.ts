5a73163e56bf4dc65803d1ddaf2f2c0e68c923e3
Efique <valentin.barriol@ynov.com>
2021-01-30 14:47:21 +0100

    token à l'inscription

--- SMI-Backend/src/Modules/users.module.ts
+++ SMI-Backend/src/Modules/users.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '../services/users.service';
 import { UsersController } from '../controllers/users.controller';
 import { User } from '../entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),     
+  JwtModule.register({
+    secret: jwtConstants.secret,
+    signOptions: { expiresIn: '3000s' },
+  }),],
   exports: [TypeOrmModule, UsersService],
   providers: [UsersService],
   controllers: [UsersController],
--- SMI-Backend/src/auth/auth.service.ts
+++ SMI-Backend/src/auth/auth.service.ts
@@ -1,30 +1,30 @@
-// import { Injectable } from '@nestjs/common';
-// import { UsersService } from '../services/users.service';
-// import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+import { JwtService } from '@nestjs/jwt';
 
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
-// @Injectable()
-// export class AuthService {
-//   constructor(
-//     private usersService: UsersService,
-//     private jwtService: JwtService,
-//   ) {}
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
-//   async validateUser(username: string, pass: string): Promise<any> {
-//     const user = await this.usersService.findOneUser(username);
-//     if (user && (await bcrypt.compare(pass, user.password))) {
-//       const { password, ...result } = user;
-//       return result;
-//     }
-//     return null;
-//   }
+  async validateUser(mail: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByMail(mail);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
 
-//   async login(user: any) {
-//     const payload = { login: user.login, sub: user.id };
-//     return {
-//       user: user.login,
-//       access_token: this.jwtService.sign(payload),
-//     };
-//   }
-// }
+  async login(user: any) {
+    const payload = { mail: user.mail, sub: user.id };
+    return {
+      user: user.mail,
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
